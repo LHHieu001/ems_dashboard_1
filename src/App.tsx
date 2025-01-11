a0c2b2481ee2dcedec2a0f8ca1a668f10f2d4b06
Hieu Le Hoang (Goatee) <lehoanghieu10a1@gmail.com>
2025-01-11 21:02:57 +0700

    Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { Tooltip } from 'react-tooltip'
 import './App.css'
+import SideBar from './components/SideBar/SideBar'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  const [activeMenu, setActiveMenu] = useState(true);
+  return(
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BrowserRouter>
+          <div className='flex relative dark:bg-main-dark-bg'>
+
+            {/* Setting Button */}
+            <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+              <button
+                type='button'
+                data-tooltip-id='Settings' data-tooltip-content="Settings" data-tooltip-place="top"
+                className='text-3xl text-white p-3 rounded-full hover:drop-shadow-xl hover:bg-light-gray'
+                style={{ background: 'blue'}}
+              >
+                <FiSettings />
+              </button>
+            </div>
+
+            {/* Side Bar */}
+            {activeMenu ? (
+                <div className='w-72 fixed sidebar bg-white dark:bg-secondary-dark-bg'>
+                  <SideBar />
+                </div>
+              ) : (
+                <div className='w-0 dark:bg-secondary-dark-bg'>
+                  <SideBar />
+                </div>
+              )}
+
+              {/* Nav Bar */}
+              <div className={`${activeMenu ? ' md:ml-72': 'flex-2'} dark:bg-main-dark-bg bg-main-bg min-h-screen w-full`}>
+                <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                  <NavBar />
+                </div>
+              </div>
+
+              <div>
+                <Routes>
+
+                  {/* DashBoard */}
+                  <Route path='/' element="home" />
+                  <Route path='/main' element="home" />
+
+                  {/* Pages */}
+                  <Route path='/users' element="User" />
+                  <Route path='/noti' element="Notification" />
+                  <Route path='/customers' element="Not used" />
+
+                  {/* Apps */}
+                  <Route path ='/kanban' element='Kanban'/>
+                  <Route path ='/editor' element='Editor'/>
+                  <Route path ='/event' element='Event'/>
+                  <Route path ='/color-picker' element='ColorPicker'/>
+
+                  {/* Detail/Monitor */}
+                  <Route path="/line" element="Line" />
+                  <Route path="/area" element="Area" />
+                  <Route path="/bar" element="Bar" />
+                  <Route path="/pie" element="Pie" />
+                  <Route path="/financial" element="Financial" />
+                  <Route path="/color-mapping" element="ColorMapping" />
+                  <Route path="/pyramid" element="Pyramid" />
+                  <Route path="/stacked" element="Stacked" />
+                
+                
+                </Routes>
+              </div>
+          </div>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Tooltip id='Settings'/>
     </>
+    
   )
 }
 
